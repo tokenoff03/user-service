a5116e53022ea6612aa5fcf8ebcb4b3c54e2f44c
ad1lek <adil.token@kmf.kz>
2024-11-29 16:26:44 +0500

    Second commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+LOCAL_BIN:=$(CURDIR)/bin
+
+install-deps:
+	GOBIN=$(LOCAL_BIN) go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.35.2
+	GOBIN=$(LOCAL_BIN) go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.5.1
+
+get-deps:
+	go get -u google.golang.org/protobuf/cmd/protoc-gen-go
+	go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+generate: 
+	make generate-user-api
+
+generate-user-api:
+	mkdir -p pkg/user_v1
+	protoc --proto_path api/user_v1 \
+	--go_out=pkg/user_v1 --go_opt=paths=source_relative \
+	--plugin=protoc-gen-go=bin/protoc-gen-go \
+	--go-grpc_out=pkg/user_v1 --go-grpc_opt=paths=source_relative \
+	--plugin=protoc-gen-go-grpc=bin/protoc-gen-go-grpc \
+	api/user_v1/user.proto
--- main.go
+++ main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"user-service/pkg/user_v1"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const grpcPort = 50051
+
+type server struct {
+	user_v1.UnimplementedUserV1Server
+}
+
+func (s *server) Get(ctx context.Context, req *user_v1.GetRequest) (*user_v1.GetResponse, error) {
+	return &user_v1.GetResponse{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listren: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	user_v1.RegisterUserV1Server(s, &server{})
+	log.Printf("Server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
